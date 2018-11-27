b8c170acf1751283a80965ccac0678c8fdb26dd2
8OnItsSide <michael.beukman@gmail.com>
2018-11-27 16:09:40 +0200

    Changed Readme to include requirements. Also changed the main_loop function to return a deque of newest 10 agents

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Rl-Interface
+
+<h2>Methods that agents need to implement:</h2>
+  These methods are necessary for this library to work.
+* pre_step(state)  
+  A function that takes in the observation and returns an action to take.
+  Gets called just before an action is taken
+
+* post_step(self, state, action, reward, next_state, done)  
+  A function that takes in the above parameters and returns nothing.
+  This is for any processing that needs doing after an action is taken
+  Gets called just after an action is taken
+
+* post_episode()  
+  This is a function that gets called after every episode, to handle post episode training and cleanup
--- RLLoop.py
+++ RLLoop.py
@@ -4,15 +4,28 @@ import datetime
 import os
 from math import log10, ceil
 import numpy as np
-
+from collections import deque
 
 class RLLoop:
-    def __init__(self, env, monitor_dir):
+    def __init__(self, env, monitor_dir, model_name='', frequency=100):
         self.env = env
-        self.monitor_dir = monitor_dir
+        self.frequency = frequency
+        self.model_name = model_name
+        if model_name == '':
+            model_dir = 'General Models'
+        else:
+            model_dir = model_name
+        self.monitor_dir = os.path.join(monitor_dir, model_dir)
 
-    @staticmethod
-    def run_episodes(env, num_episodes, agent, verbose=False):
+    def run_episodes(self, env, num_episodes, agent, verbose=False):
+        """
+
+        :param env: open Ai gym environment
+        :param num_episodes: Number of episodes to run
+        :param agent: A reinforcement learning agent. It has to have at least the methods specified in the readme
+        :param verbose: Boolean indicating if the rewards should be outputted
+        :return: a tuple of (episode_rewards, agent)
+        """
         episode_rewards = []
         for episode in range(1, num_episodes+1):
             state = env.reset()
@@ -25,9 +38,11 @@ class RLLoop:
                 episode_reward += reward
                 if done:
                     break
-            if verbose:
-                print("Reward at episode {} : {}".format(episode, episode_reward))
             episode_rewards.append(episode_reward)
+            if verbose:
+                if episode % self.frequency == 0:
+                    print("Average reward over {} episodes at episode {} : {}"
+                          .format(self.frequency, episode, sum(episode_rewards[episode-self.frequency:])/self.frequency))
             agent.post_episode()
         return episode_rewards, agent
 
@@ -40,9 +55,20 @@ class RLLoop:
         return os.path.join(date_str + " " + time_str)
 
     def do_multiple_runs(self, num_runs, agent,num_episodes, doMonitor=True, seeding=True, verbose=False):
+        """
+
+        :param num_runs: Number of different runs to do
+        :param agent: A reinforcement learning agent. Refer to the readme for the methods the agent needs to have
+        :param num_episodes: Number of episodes per run
+        :param doMonitor: Should the data be saved using openAI gym monitor
+        :param seeding: Should the env be seeded with the run number (0, 1, ..., num_runs-1)
+        :param verbose: Should the rewards be printed to the screen
+        :return: tuple of (all_episode_rewards, deque of 10 newest agents)
+        """
         monitor_folder_name = os.path.join(self.monitor_dir, self.get_monitor_folder_name(self))
         num_zeros = int(ceil(log10(num_runs)))
         all_episode_rewards = []
+        newest_agents = deque(maxlen=10)
         for run_num in range(num_runs):
             folder = os.path.join(monitor_folder_name, str(run_num).zfill(num_zeros))
             if doMonitor:
@@ -51,10 +77,11 @@ class RLLoop:
                 monitor = self.env
             if seeding:
                 monitor.seed(run_num)
-            episode_rewards, _ = self.run_episodes(monitor, num_episodes, agent, verbose=verbose)
+            episode_rewards, new_agent = self.run_episodes(monitor, num_episodes, agent, verbose=verbose)
+            newest_agents.append(new_agent)
             all_episode_rewards.append(episode_rewards)
 
-        return all_episode_rewards
+        return all_episode_rewards, newest_agents
 
     @staticmethod
     def analyse_rewards(self, episode_rewards):
@@ -72,5 +99,3 @@ class RLLoop:
                "totalAverage":total_average}
 
         return dic
-
-
